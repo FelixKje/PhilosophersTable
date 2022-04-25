8648ec3bb7574a5700144cc772fd61cbb1740f62
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-04-25 13:03:55 +0200

    Program now works but we need more rules

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading;
 
-for (int i = 0; i < 10; i++){
-    Console.WriteLine(i%5);
-}
+Philosopher aristotle = new Philosopher(Table.Copper, Table.Platinum, "Aristotle", 4);
+Philosopher sokratis = new Philosopher(Table.Platinum, Table.Gold, "Sokratis", 6);
+Philosopher john = new Philosopher(Table.Gold, Table.Silver, "John", 7);
+Philosopher augustine = new Philosopher(Table.Silver, Table.Wood, "Augustine", 5);
+Philosopher thomas = new Philosopher(Table.Wood, Table.Copper, "Thomas", 4);
+
+new Thread(aristotle.Think).Start();
+new Thread(sokratis.Think).Start();
+new Thread(john.Think).Start();
+new Thread(augustine.Think).Start();
+new Thread(thomas.Think).Start();
+
+Console.ReadKey();
+
 
 enum PhilosopherState { Eating, Thinking }
 class Philosopher{
@@ -13,12 +26,14 @@ class Philosopher{
 
     readonly int StarvationThreshold;
 
+    int timesEaten = 0;
+
     public readonly Chopstick RightChopstick;
     public readonly Chopstick LeftChopstick;
 
     Random rand = new Random();
 
-    int contThinkCOunt = 0;
+    int contThinkCount = 0;
 
     public Philosopher(Chopstick rightChopstick, Chopstick leftChopstick, string name, int starvationThreshold){
         RightChopstick = rightChopstick;
@@ -26,6 +41,37 @@ class Philosopher{
         Name = name;
         State = PhilosopherState.Thinking;
         StarvationThreshold = starvationThreshold;
+    }
+
+    public void Eat(){
+        if (TakeChopstickInRightHand()){
+            if (TakeChopstickInLeftHand()){
+                this.State = PhilosopherState.Eating;
+                timesEaten++;
+                Console.WriteLine($"{Name} is eating for the {timesEaten}th time, with {RightChopstick.ChopStickID} and {LeftChopstick.ChopStickID}");
+                Thread.Sleep(rand.Next(5000,10000));
+
+                contThinkCount = 0;
+                
+                RightChopstick.Put();
+                LeftChopstick.Put();
+            }
+        }
+
+        Think();
+    }
+
+    public void Think(){
+        this.State = PhilosopherState.Thinking;
+        Console.WriteLine($"{Name} is thinking...on {Thread.CurrentThread.Priority.ToString()}");
+        Thread.Sleep(rand.Next(2500, 20000));
+        contThinkCount++;
+
+        if (contThinkCount > StarvationThreshold){
+            Console.WriteLine($"{Name} is starving");
+        }
+        
+        Eat();
     }
 
     private bool TakeChopstickInLeftHand() => LeftChopstick.Take(Name);
@@ -58,4 +104,18 @@ class Chopstick{
         Console.WriteLine($"{ChopStickID} is placed on the table by {TakenBy}");
         TakenBy = String.Empty;
     }
+
+   
+}
+class Table{
+    internal static Chopstick Platinum = new Chopstick()
+        {ChopStickID = "Platinum Chopstick", State = ChopstickState.onTable};
+    internal static Chopstick Gold = new Chopstick()
+        {ChopStickID = "Gold Chopstick", State = ChopstickState.onTable};
+    internal static Chopstick Silver = new Chopstick()
+        {ChopStickID = "Silver Chopstick", State = ChopstickState.onTable};
+    internal static Chopstick Wood = new Chopstick()
+        {ChopStickID = "Wood Chopstick", State = ChopstickState.onTable};
+    internal static Chopstick Copper = new Chopstick()
+        {ChopStickID = "Copper Chopstick", State = ChopstickState.onTable};
 }
